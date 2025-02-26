8d9461096cd89d9b1850e5cc39414a95046368d0
Amada07 <amadacarcamo938@gmail.com>
2025-02-26 01:31:58 -0600

    creacion codigo

--- BancoApp/src/bancoapp/BancoApp.java
+++ BancoApp/src/bancoapp/BancoApp.java
@@ -14,7 +14,17 @@ public class BancoApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        CuentaBancaria cuenta1 = new CuentaBancaria (123456,500);
+        
+        System.out.println("Cuenta creada:"+ cuenta1.getNumeroCuenta());
+        System.out.println("Saldo Inicial:$" + cuenta1.getSaldo());
+   
+    cuenta1.depositar(200);
+    cuenta1.retirar(100);
+    cuenta1.retirar(700);
+    
+    System.out.println("Saldo Final:$"+ cuenta1.getSaldo());
     }
     
 }
--- BancoApp/src/bancoapp/CuentaBancaria.java
+++ BancoApp/src/bancoapp/CuentaBancaria.java
@@ -11,4 +11,41 @@ package bancoapp;
 public class CuentaBancaria {
     //Atributos privados
     private int numeroCuenta;
+    private double saldo;
+    
+    public CuentaBancaria(int numeroCuenta, double saldoInicial){
+        this.numeroCuenta=numeroCuenta;
+        this.saldo = ( saldoInicial >= 0)? saldoInicial:0;
+    
+        }
+    
+    //Metodo para depositar
+    public void depositar(double cantidad){
+        if(cantidad > 0){
+            saldo +=cantidad;
+            System.out.println("Deposito exitoso: $"+ cantidad);
+        }else{
+            System.err.println("Error:La cantidad a depositar debe ser positivo");
+        }
+    }
+    
+    //Metodo para retirar
+    public void retirar(double cantidad){
+        if(cantidad > 0 && cantidad <=saldo){
+            saldo -=cantidad;
+            System.out.println("Retiro exitoso:$" + cantidad);
+        }else{
+           System.err.println("Error: Retiro invalido.Fondos insuficientes");
+        }
+    }
+
+    //Metodo consultar saldo 
+    public double getSaldo(){
+        return saldo;
+    }
+    
+    //Metodo para obtener numero de cuenta
+    public int getNumeroCuenta(){
+        return numeroCuenta;
+    }
 }
